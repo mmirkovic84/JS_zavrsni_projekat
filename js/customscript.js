3a732d55c5963f0380efafbf47ba2b3fb9e941cc
Marko Mirkovic <mirkovic.m84@gmail.com>
2023-08-02 23:28:39 +0200

    Fixed Radio buttons function.

--- js/customscript.js
+++ js/customscript.js
@@ -3,7 +3,7 @@ function replace1(hide, show) {
   document.getElementById(show).style.display = "block";
 }
 
-function replace2() {
+function replaceFirstPage() {
   document.getElementById('div2').style.display = "none";
   document.getElementById('div3').style.display = "block";
 }
@@ -18,24 +18,23 @@ function checkName() {
     alert("Please enter your name to continue!!!");
   }
   else {
-    replace2();
+    replaceFirstPage();
   }
-  
 }
 
 // dodavanje text boxa ispod radio buttona
-function showText(num){
-  if (num == 7) { 
-    document.getElementById('addTextArea').style.display='block';
+function showText() {
+  var radio7 = document.querySelector("#Radio7").checked;
+  if (radio7 == true) {
+    document.getElementById('addTextArea').style.display = 'block';
   }
   else {
-    document.getElementById('addTextArea').style.display='none';
+    document.getElementById('addTextArea').style.display = 'none';
   }
-  return;
 }
 
 // swichovanje finalnih poruka
-function replace3() {
+function replaceFinalResults() {
   document.getElementById('div6').style.display = "none";
   document.getElementById('final1').style.display = "block";
 }
@@ -43,36 +42,36 @@ function replace3() {
 function FinalMessage() {
   var textFinal = document.querySelector(".Text_final");
   var smileyFinal = document.querySelector(".Smiley_final");
-  var rangeResult = document.getElementById("customRange2").value; 
+  var rangeResult = document.getElementById("customRange2").value;
 
   switch (rangeResult) {
-    case "1": 
-    replace3();
-    textFinal.innerHTML = "Oops, Sorry to disapoint you.";
-    smileyFinal.innerHTML = '<i class="bi bi-emoji-frown-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
-    break;
-    case "2": 
-    replace3();
-    textFinal.innerHTML = "I hope we will be better next time.";
-    smileyFinal.innerHTML = '<i class="bi bi-emoji-expressionless-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
-    break;
-    case "3": 
-    replace3();
-    textFinal.innerHTML = "Golden Middle.";
-    smileyFinal.innerHTML = '<i class="bi bi-emoji-smile-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
-    break;
-    case "4": 
-    replace3();
-    textFinal.innerHTML = "Thanks for the very good rating.";
-    smileyFinal.innerHTML = '<i class="bi bi-emoji-laughing-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
-    break;
-    case "5": 
-    replace3();
-    textFinal.innerHTML = "Perfect. We are glad to meet your requirements.";
-    smileyFinal.innerHTML = '<i class="bi bi-emoji-heart-eyes-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
-    break;
-    default:'Error 404. Not Found!';
-    break;
+    case "1":
+      replaceFinalResults();
+      textFinal.innerHTML = "Oops, Sorry to disapoint you.";
+      smileyFinal.innerHTML = '<i class="bi bi-emoji-frown-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
+      break;
+    case "2":
+      replaceFinalResults();
+      textFinal.innerHTML = "I hope we will be better next time.";
+      smileyFinal.innerHTML = '<i class="bi bi-emoji-expressionless-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
+      break;
+    case "3":
+      replaceFinalResults();
+      textFinal.innerHTML = "Golden Middle.";
+      smileyFinal.innerHTML = '<i class="bi bi-emoji-smile-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
+      break;
+    case "4":
+      replaceFinalResults();
+      textFinal.innerHTML = "Thanks for the very good rating.";
+      smileyFinal.innerHTML = '<i class="bi bi-emoji-laughing-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
+      break;
+    case "5":
+      replaceFinalResults();
+      textFinal.innerHTML = "Perfect. We are glad to meet your requirements.";
+      smileyFinal.innerHTML = '<i class="bi bi-emoji-heart-eyes-fill" style="font-size: 6rem; color: rgb(255, 255, 255);"></i>';
+      break;
+    default: 'Error 404. Not Found!';
+      break;
   }
 }
 
